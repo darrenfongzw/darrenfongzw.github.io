1873abad1e7b4e982ea5f135375002a86480e05e
Darren <fong.zwd@gmail.com>
2020-05-12 22:17:20 +0800

    init

--- deploywebsite.sh
+++ deploywebsite.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # update resume & deploy newest website to git
 
-git -C ~/WebstormProjects/Resume-plain/ pull
-pdflatex ~/WebstormProjects/Resume-plain/mmeyer.tex 
-mv ~/WebstormProjects/Resume-plain/mmeyer.pdf ~/WebstormProjects/Resume-plain/resume.pdf
-cp ~/WebstormProjects/Resume-plain/resume.pdf ~/WebstormProjects/darrenfongzw.github.io/
-git -C ~/WebstormProjects/darrenfongzw.github.io/resume.pdf add
-git -C ~/WebstormProjects/darrenfongzw.github.io/ commit "update resume"
-git -C ~/WebstormProjects/darrenfongzw.github.io/ push
+git -C /home/darren/WebstormProjects/Resume-plain/ pull
+cd /home/darren/WebstormProjects/Resume-plain
+pdflatex mmayer.tex
+cd /home/darren/WebstormProjects/darrenfongzw.github.io/
+mv /home/darren/WebstormProjects/Resume-plain/mmayer.pdf /home/darren/WebstormProjects/Resume-plain/resume.pdf
+cp /home/darren/WebstormProjects/Resume-plain/resume.pdf /home/darren/WebstormProjects/darrenfongzw.github.io/
+git add /home/darren/WebstormProjects/darrenfongzw.github.io/resume.pdf
+git -C /home/darren/WebstormProjects/darrenfongzw.github.io/ commit -m "update resume"
+git -C /home/darren/WebstormProjects/darrenfongzw.github.io/ push
 
